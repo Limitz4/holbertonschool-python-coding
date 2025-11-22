752121256ffb5d53c1481bda84c1d206bf6939da
Limitz4 <qafarlicingiz18@gmail.com>
2025-11-22 11:53:35 +0000

    Fix 0-square.py with complete documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""This is a module for Square class"""
+
+
 class Square:
+    """This is a class that defines a square"""
+
     def __init__(self, size):
+        """This is the constructor method
+        Args:
+            size: the size of the square
+        """
         self.__size = size
