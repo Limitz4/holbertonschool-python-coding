105e104ef741d722e3f0bd3de88596013db035fa
Limitz4 <qafarlicingiz18@gmail.com>
2025-11-22 11:56:09 +0000

    Add 2-square.py class with area method

---
+++ python-classes/2-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Square class module"""
+
+
+class Square:
+    """Square class that defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square
+        
+        Args:
+            size: The size of the square (default 0)
+            
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate and return the area of the square
+        
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
