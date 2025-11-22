4cb93b69d5d9e33474c90065de857a8cde82d855
Limitz4 <qafarlicingiz18@gmail.com>
2025-11-22 11:49:15 +0000

    Fix 0-square.py with complete documentation and PEP8

--- pyhton-classes/0-square.py
+++ pyhton-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Square class module"""
+
+
 class Square:
+    """Square class that defines a square"""
+
     def __init__(self, size):
+        """Initialize a new Square
+        
+        Args:
+            size: The size of the square
+        """
         self.__size = size
